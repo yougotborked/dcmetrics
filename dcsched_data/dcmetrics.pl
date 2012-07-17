1439cf3964d0fafaf2b440f5fcc0a59e9c63186d
abork <abork@cray.com>
2012-07-17 10:04:48 -0500

    Converted to Usage instead of parsing the output of a general dcsched query. Aka (dcsched -u) intead of (dcsched whatever)

--- dcsched_data/dcmetrics.pl
+++ dcsched_data/dcmetrics.pl
@@ -7,8 +7,7 @@ use Chart::Pie;
 use GD::Graph::pie;
 use List::Util 'max';
 
-
-my $debuG = 1;
+my $debug = 1;
 
 my %oh = ();
 $oh{verbose} = '';
@@ -16,14 +15,12 @@ $oh{all}     = '';
 $oh{date}    = '';
 $oh{group}   = '';
 $oh{machine} = '';
-my $command = '';
+my $command = ' -u ';
 
 GetOptions(
 	'verbose!'  => \$oh{verbose},
 	'all'       => \$oh{all},
 	'date=s'    => \$oh{date},
-	'group=s'   => \$oh{group},
-	'machine=s' => \$oh{machine}
   );
 
 # Perl trim function to remove whitespace from the start and end of the string
@@ -51,10 +48,6 @@ sub rtrim($) {
 #foreach (keys %oh) {
 # print "$_ = $oh{$_}\n";
 #}
-print "Unprocessed by Getopt::Long\n" if $ARGV[0];
-foreach (@ARGV) {
-	print "$_\n";
-}
 
 if ( $oh{verbose} ) {
 	$v = '-v';
@@ -74,46 +67,45 @@ if ( $oh{date} ) {
 	$command .= ' -' . $date . ' -s ' . '-' . $date . ' ';
 }
 
-if ( $oh{group} ) {
-	$group = $oh{group};
-	print "group set\n";
-	$command .= ' ' . $group . ' ';
+my $extraArgs;
+print "Unprocessed by Getopt::Long\n" if $ARGV[0];
+foreach (@ARGV) {
+	print $_."\n";
+	$extraArgs .= ' '.$_;
 }
 
-if ( $oh{machine} ) {
-	$machine = $oh{machine};
-	print "machine set\n";
-	$command .= ' ' . $machine . ' ';
-}
+$command .= $extraArgs;
 
 printf "|%s|\n", $command;
 
-@output = ();
 open( DCPIPE, "/sw/sdev/dcsched/dcsched $command |" );
 $i = 1;
 %timeHash = ();
-
+my $dateRange;
+$extraArgs = trim($extraArgs);
 while (<DCPIPE>) {
-	if ( $_ =~ m/until/ ) {
-		$temp = ltrim($_);
-		$temp =~ s/^\*+//;
-		$temp =~ s/until\s+//;
-		$temp = ltrim($temp);
-		
-		my @col = split /\s/, $temp;
-		
-		if ( $debuG > 0 ) {print $col[0]." ".$col[1]." ".$col[2]." ".$col[3]." ".$col[4]." ".$col[5]." ".$col[6]."\n";}
-		
-		$time1    = str2time( $col[0] . " " . $col[1] . " " . $col[2] );
-		$time2    = str2time( $col[3] . " " . $col[4] . " " . $col[5] );
-		$category = $col[6];
-		
-		$timeHash{$category} += $time2 - $time1;
-		
-		$output[$i] ="time1: " . $time1 . " time2: " . $time2 . " category: " . $category . "\n";
-		print $output[$i];
+	if (($_ =~ m/Summary system usage report for/)  || ($_ =~ m/System usage report for ($extraArgs)/)) {
+		$dateRange = $_;
+		$dateRange =~ s/^[\w\s]+\(/\(/;
+		while (<DCPIPE>) {
+			if ($_ =~ m/Scheduled time/ || $_ =~ m/Scheduled hours/) {	
+				while (<DCPIPE>) {
+					last if $_ =~ m/Total/;
+					if ($_ =~ m/\w\s*\w\s*\w/) {
+						if ($debug > 1) {
+							print $_;
+						}
+						$temp = trim($_);
+						my @col = split /\s+/, $temp;
+						if ( $debug > 0 ) {print $col[0]." ".$col[1]." ".$col[2]."\n";}
+						$category = $col[2];
+						$time = $col[0];
+						$timeHash{$category} += $time
+					}	
+				}
+			}
+		}
 	}
-	$i++;
 }
 
 while ( ( $key, $value ) = each(%timeHash) ) {
@@ -124,35 +116,35 @@ close DCPIPE;
 
 my @timeData = ([keys %timeHash],[values %timeHash]);
 
-## CReATE THE CHART !!!!!___
-#GD
+### CReATE THE CHART !!!!!___
+##GD
+#
+#open( PNGFILE, ">./graph.png" ) || die "Cannot open graph.png for write: $!\n";
+#binmode PNGFILE;
+#
+## Both the arrays should same number of entries.
+#my $mygraph = GD::Graph::pie->new( 300, 300 );
+#$mygraph->set(
+#	title => $oh{machine} . " usage information",
+#
+#	#  '3d'          => 1,
+#  ) or warn $mygraph->error;
+#$mygraph->set_value_font(GD::gdMediumBoldFont);
+#
+#my $myimage = $mygraph->plot( \@timeData ) or die $mygraph->error;
+#
+#print "Content-type: image/png\n\n";
+#print PNGFILE $myimage->png;
+#close(PNGFILE);
 
-open( PNGFILE, ">./graph.png" ) || die "Cannot open graph.png for write: $!\n";
-binmode PNGFILE;
-
-# Both the arrays should same number of entries.
-my $mygraph = GD::Graph::pie->new( 300, 300 );
-$mygraph->set(
-	title => $oh{machine} . " usage information",
-	#  '3d'          => 1,
-  ) or warn $mygraph->error;
-$mygraph->set_value_font(GD::gdMediumBoldFont);
-
-my $myimage = $mygraph->plot( \@timeData ) or die $mygraph->error;
-
-print "Content-type: image/png\n\n";
-print PNGFILE $myimage->png;
-close(PNGFILE);
-
-#CHART
+#___CHART
 
 my $chart = Chart::Pie->new (600,600);
-$chart->set('title' => $oh{machine} . " usage information");
+$chart->set('title' => $extraArgs . " usage information from ". $dateRange);
 $chart->add_dataset( keys %timeHash );
 $chart->add_dataset( values %timeHash);
 
 $chart->png('output.png');
-
 
 #DONE
 print "All done!\n";

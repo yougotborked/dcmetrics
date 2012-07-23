25b42d3eeb6ba8cd76e4bd601dd66703b82678a1
abork <abork@cray.com>
2012-07-23 13:52:23 -0500

    Fixed Inconsistencys between -v option and -u. Now the only problem is the -v usually gives a start date "Inside" the last slot, rather than starting at midnight on the start date.

--- dcsched_data/dcmetrics.pl
+++ dcsched_data/dcmetrics.pl
@@ -2,8 +2,10 @@
 use warnings;
 use Getopt::Long;
 use Date::Parse;
+use Time::Local;
 use CGI ':standard';
 use Chart::Pie;
+use Chart::StackedBars;
 use GD::Graph::pie;
 use List::Util 'max';
 
@@ -152,27 +154,44 @@ print "Usage Done!\n";
 open( OTHERPIPE, "/sw/sdev/dcsched/dcsched $command |" );
 $i = 0;
 %timeHash2 = ();
+%osHash = ();
 my $time1;
 my $time2;
 my $category;
+my $reservedOS;
+my $userToggle =0;
 while (<OTHERPIPE>) {
-	if ($debug > 1 ) {print $_;}
+	if ($debug > 2 ) {print $_;}
 	if ( $_ =~ m/until/ ) {
-		if ($debug > 0) {
-			print $_;
+		$userToggle = 0;
+		if ($_ =~ m/\)/ ) {$userToggle = 1}
+		if ($debug > 1) {
+			print trim($_)."\n";
 		}
+		undef $time1;
+		undef $time2;
+		undef $reservedOS;
+		undef $category;
 		while (<OTHERPIPE>) {
+			if ($debug > 0) {print $_;}
 			$temp = trim($_);
 			my @col = split /=/, $temp;
 			if ($col[0] =~ m/StartDate/) {$time1 = str2time($col[1]);}
 			if ($col[0] =~ m/EndDate/) {$time2 = str2time($col[1]);}
 			if ($col[0] =~ m/SkSlotName/) {$category = trim($col[1]);}
-			last if $_ =~ m/until/;
+			if ($col[0] =~ m/UrOsVersion/) {$reservedOS = trim($col[1]);}
+			if ($userToggle == 0) {last if $col[0] =~ m/SkSoftwareTypes/; # last item in First half
+			}
+			elsif ($userToggle == 1) {last if $col[0] =~ m/UrSurrender/; #last item in Second Half
+			}
 		}
-		$output[$i] ="time1: " . $time1 . " time2: " . $time2 . " category: " . $category . "\n";
-		if ($debug > 1) {print $output[$i];}
-		print $time1." ".$time2."\n";
-		$timeHash2{$category} += ($time2 - $time1)/60/60;
+
+		$timeDiff = ($time2-$time1)/60/60;
+		$timeHash2{$category} += $timeDiff;
+		if ($reservedOS) {$osHash{$reservedOS} += $timeDiff;}
+		if ($reservedOS) {$output[$i] ="Time: ".$timeDiff. " category: " . $category . " os: ". $reservedOS. " \n";}
+		else {$output[$i] ="Time: ".$timeDiff. " category: " . $category. " \n";}
+		if ($debug > 0) {print $output[$i];}
 		$i++;
 	}
 }
@@ -191,5 +210,19 @@ $chart2->add_dataset( keys %timeHash2 );
 $chart2->add_dataset( values %timeHash2);
 
 $chart2->png('output_machine.png');
+
+my $chart3 = Chart::Pie->new (900,900);
+$chart3->set('title' => $extraArgs . " Os information from " . $dateRange);
+$chart3->add_dataset( keys %osHash);
+$chart3->add_dataset( values %osHash);
+
+$chart3->png('output_OS.png');
+
+my $chart4 = Chart::StackedBars->new(900,900);
+$chart4->set('title' => $extraArgs. " Os and Usage Info from " . $dateRange);
+$chart4->add_dataset (keys %timeHash2);
+$chart4->add_dataset (values %timeHash2);
+$chart4->add_dataset (values %timeHash2);
+$chart4->png('output_stacked.png');
 
 print "machine Done!";
